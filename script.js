35b02749959e9bc655184bc1bc12ba1b4e38e0ab
nicolasmeller <56871907+nicolasmeller@users.noreply.github.com>
2022-03-26 12:48:56 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function draw() {
+    const canvas = document.querySelector('#canvas');
+
+    if (!canvas.getContext) {
+        return;
+    }
+    const ctx = canvas.getContext('2d');
+
+  
+    const pointArray = [];
+    for (let i = 0; i < 200; i++) {
+      var x = Math.floor(Math.random() * 490);
+      var y = Math.floor(Math.random() * 490);
+      const point = {x: x, y: y}
+      pointArray.push(point)
+
+    }
+    pointArray.forEach(point => 
+      ctx.fillRect(point.x, point.y, 10, 10));
+
+
+    
+    
+    
+}
+
+draw();
