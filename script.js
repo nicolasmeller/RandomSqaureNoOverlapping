f392b50153d6d39e7c00c960eb4ca8d5442ba662
nicolasmeller <56871907+nicolasmeller@users.noreply.github.com>
2022-03-26 13:36:30 +0000

    changes

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-function draw() {
-    const canvas = document.querySelector('#canvas');
-
+//getting canvas from html
+const canvas = document.querySelector('#canvas');
+const ctx = canvas.getContext('2d');
+function getCanvas(){
     if (!canvas.getContext) {
         return;
     }
-    const ctx = canvas.getContext('2d');
+}
+//distance between to points
+function dist(x1, y1, x2, y2){
+    let y = x2 - x1;
+    let x = y2 - y1;
+    
+    return Math.sqrt(x * x + y * y);
+}
+//drawing the squares 
+function draw() {
+   
+    getCanvas();
 
-  
     const pointArray = [];
     for (let i = 0; i < 200; i++) {
       var x = Math.floor(Math.random() * 490);
@@ -16,7 +27,7 @@ function draw() {
 
     }
     pointArray.forEach(point => 
-      ctx.fillRect(point.x, point.y, 100, 10));
+      ctx.fillRect(point.x, point.y, 10, 10));
 
 
     
