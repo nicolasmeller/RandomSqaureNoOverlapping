cb249fa08f9098b0170f8ff439792858430bfd40
nicolasmeller <56871907+nicolasmeller@users.noreply.github.com>
2022-03-27 10:28:52 +0000

    no overlapping. if they overlap then the for loop breaks and continue to the next square

--- script.js
+++ script.js
@@ -1,38 +1,33 @@
-//getting canvas from html
-const canvas = document.querySelector('#canvas');
-const ctx = canvas.getContext('2d');
-function getCanvas(){
-    if (!canvas.getContext) {
-        return;
+var squares = [];
+
+function setup(){
+  createCanvas(600,600);
+  
+  for (let i = 0; i < 200; i++) {
+      var square = {
+      x: Math.floor(Math.random() * 590),
+      y: Math.floor(Math.random() * 590),
+      s: 10
+      }
+    var overlapping = false;
+    for(var j = 0; j < squares.length; j++)     {
+      var other = squares[j];
+      var d = dist(square.x , square.y , other.x, other.y);
+      if(d < square.s + other.s){
+        overlapping = true;
+        break;
+      } 
     }
+    if(!overlapping){
+       squares.push(square);
+    }
+    }
+  for (var i = 0; i < squares.length; i++){
+    fill(0, 0, 0);
+    rect(squares[i].x, squares[i].y, squares[i].s, squares[i].s);
+  }
+  
 }
-//distance between to points
-function dist(x1, y1, x2, y2){
-    let y = x2 - x1;
-    let x = y2 - y1;
-    
-    return Math.sqrt(x * x + y * y);
-}
-//drawing the squares 
 function draw() {
-   
-    getCanvas();
-
-    const pointArray = [];
-    for (let i = 0; i < 200; i++) {
-      var x = Math.floor(Math.random() * 490);
-      var y = Math.floor(Math.random() * 490);
-      const point = {x: x, y: y}
-      pointArray.push(point)
-
-    }
-    pointArray.forEach(point => 
-      ctx.fillRect(point.x, point.y, 10, 10));
-
-
-    
-    
-    
+  
 }
-
-draw();
